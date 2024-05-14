bfa04d9b42eedef62cd43d1782728f28c78e4358
Erick Jonathan <zxjonathanxz22@gmail.com>
2024-05-14 17:55:22 -0300

    imagens da animação add

--- personagens/aldeao/imgfundobranco.py
+++ personagens/aldeao/imgfundobranco.py
@@ -1,20 +1,29 @@
 import os
 from rembg import remove
-from PIL import Image
+from PIL import Image, ImageOps
 
-caminho = "C:/Users/User/Documents/Estudo/Werewolf-Mystery/personagens/aldeao/output"
+caminho = "C:/Users/User/Documents/Estudo/Werewolf-Mystery/personagens/aldeao/output/SemFundo_jump_eixo_x"
 formato = ".png"
-frames = 80
-saida = "SemFundo"
+frames = 110
+saida = "SemFundo_jump_eixo_espelhado"
 
 # Create the directory if it doesn't exist
 if not os.path.exists(f"{caminho}/{saida}"):
     os.makedirs(f"{caminho}/{saida}")
 
+# for i in range(frames):
+#     id = i+1
+#     caminhoimagem = f"{caminho}/{id:04d}{formato}"
+#     imgemarq = Image.open(caminhoimagem)
+#     imagesembg = remove(imgemarq)
+#     print(f"Salvando {id:04d}{formato}")
+#     imagesembg.save(f"{caminho}/{saida}/{id:04d}{formato}")
+
 for i in range(frames):
     id = i+1
     caminhoimagem = f"{caminho}/{id:04d}{formato}"
     imgemarq = Image.open(caminhoimagem)
+    imgemarq = ImageOps.mirror(imgemarq)  # Espelha a imagem
     imagesembg = remove(imgemarq)
     print(f"Salvando {id:04d}{formato}")
     imagesembg.save(f"{caminho}/{saida}/{id:04d}{formato}")
